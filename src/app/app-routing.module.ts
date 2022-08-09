96ca0ea0da149d031b99644d447c900791c61460
Eduardo Professor <eduardocostaprofessor@gmail.com>
2022-08-09 18:18:02 -0300

    Inicializa o projeto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+// imports de componentes
+import { LoginComponent } from './views/login/login.component';
+import { ProdutoComponent } from './views/produto/produto.component';
+import { HomeComponent } from './views/home/home.component';
+
+
+const routes: Routes = [
+  { path: "login", component: LoginComponent },
+  {path: "produto", component: ProdutoComponent},
+  {path: "home", component: HomeComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
+
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './views/login/login.component';
+import { ProdutoComponent } from './views/produto/produto.component';
+import { HomeComponent } from './views/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ProdutoComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
